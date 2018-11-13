c9dbf3c07f87e57ebe9915fce449275971658f0e
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-12 19:54:28 -0600

    Starting point app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Places to go
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
