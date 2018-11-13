b82c7439220580358c3ddef8a8f50a326170f761
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-13 13:52:53 -0600

    Get geolocation and render Venue

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';
+import { CLIENT_ID, CLIENT_SECRET } from './../constants/api-key';
+import Venue from './Venue';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      venues: [],
+      latlong: ''
+    };
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition(response => {
+      this.setState({
+        latlong: `${response.coords.latitude},${response.coords.longitude}`
+      }, () => {
+        this.getVenues();
+      });
+    }, err => {
+      console.warn('ERROR(' + err.code + '): ' + err.message);
+    });
+  }
+
+  getVenues() {
+    const venuesEndpoint = 'https://api.foursquare.com/v2/venues/explore?';
+
+    const params = {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      limit: 5,
+      query: 'tacos',
+      v: '20181112',
+      ll: this.state.latlong
+    };
+
+    fetch(venuesEndpoint + new URLSearchParams(params), {
+      method: 'GET'
+    }).then(response => response.json()).then(response => {
+      console.log(response.response.groups[0].items);
+      this.setState({venues: response.response.groups[0].items});
+    });
+
+  }
+
   render() {
+    let venueList = this.state.venues.map((item, i) =>
+      <Venue key={i}
+          name={item.venue.name}
+          location={item.venue.location.address}
+      />
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          Places to go
-        </header>
+        <ul>
+          {venueList}
+        </ul>
       </div>
     );
   }
---
+++ src/components/Venue.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Venue = ({ name, location }) => <li>{name}, {location}</li>;
+
+export default Venue;
