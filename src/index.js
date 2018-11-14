509de497daa31a933c25d127237e08a909243dcc
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-14 10:24:25 -0600

    Set up sass and styles folder

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import './styles/css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
