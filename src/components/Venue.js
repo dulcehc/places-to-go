8b2ad6310882db5285b880524caf506c558bc4b7
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-14 19:43:17 -0600

    Get photo and rating from venues endpoint

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FaHeart } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
 import { CLIENT_ID, CLIENT_SECRET } from './../constants/api-key';
 import { DEFAULT_PHOTO } from '../constants/images';
 
@@ -14,60 +14,54 @@ class Venue extends Component {
     super(props);
     this.state = {
       photo: DEFAULT_PHOTO,
-      likes: 0
+      rating: 0
     }
   }
 
   componentDidMount() {
-    this.getPhoto(this.props.id);
-    this.getLikes(this.props.id);
+    this.getInformation(this.props.id);
   }
 
-  getPhoto = id => {
-    const photosEndpoint = `https://api.foursquare.com/v2/venues/${id}/photos?`;
-    fetch(photosEndpoint + new URLSearchParams(params), {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.id !== this.props.id) {
+      this.setState({
+        photo: DEFAULT_PHOTO,
+        rating: 0
+      });
+      this.getInformation(nextProps.id);
+    }
+  }
+  getInformation = id => {
+    const endpoint = `https://api.foursquare.com/v2/venues/${id}?`;
+    fetch(endpoint + new URLSearchParams(params), {
       method: 'GET'
     }).then(response => response.json())
       .then(res => {
-        const url = res.response.photos.items[0];
+        const info = res.response.venue;
         this.setState({
-          photo: `${url.prefix}original${url.suffix}`,
+          photo: `${info.bestPhoto.prefix}original${info.bestPhoto.suffix}` || DEFAULT_PHOTO,
+          rating: info.rating
         });
       })
       .catch(error => {
         console.log('Error: ', error)
     });
   }
-
-  getLikes = id => {
-    const likesEndpoint = `https://api.foursquare.com/v2/venues/${id}/likes?`;
-    fetch(likesEndpoint + new URLSearchParams(params), {
-      method: 'GET'
-    }).then(response => response.json())
-      .then(res => {
-       this.setState({
-          likes: res.response.likes.count,
-        });
-      })
-      .catch(error => {
-        console.log('Error: ', error)
-    });
-  }
-
 
   render() {
-    const { photo, likes } = this.state;
+    const { photo, rating } = this.state;
     const { name, location } = this.props;
 
     return (
       <div className="Venue">
         <img className="Venue__image" src={photo} />
         <span className="Venue__title">{name}</span>
-        <span className="Venue__location">{location}</span>
+        <span className="Venue__location">
+          {location && `Address: ${location}`}
+        </span>
         <span className="Venue__social">
-
-        <FaHeart color='red' size='1.5rem'></FaHeart>
-        {likes}
+          <FaStar color='yellow' size='1.5rem'></FaStar>
+          {rating}
         </span>
       </div>
     );
