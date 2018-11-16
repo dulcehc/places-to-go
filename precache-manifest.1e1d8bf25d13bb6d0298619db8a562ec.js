f73552f8a58f6ec42ec723f230b8ee40a8792626
Dulce Hernandez <dulce.hernandezc@gmail.com>
2018-11-16 15:33:08 -0600

    Updates

---
+++ precache-manifest.1e1d8bf25d13bb6d0298619db8a562ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7925c51c59146f9b8a4d",
+    "url": "/places-to-go/static/js/runtime~main.7925c51c.js"
+  },
+  {
+    "revision": "c25945b73554499dffeb",
+    "url": "/places-to-go/static/js/main.c25945b7.chunk.js"
+  },
+  {
+    "revision": "c089eefecabf100bbc32",
+    "url": "/places-to-go/static/js/1.c089eefe.chunk.js"
+  },
+  {
+    "revision": "c25945b73554499dffeb",
+    "url": "/places-to-go/static/css/main.469f4676.chunk.css"
+  },
+  {
+    "revision": "74b20441347501c5484b96a9b68a5a1e",
+    "url": "/places-to-go/index.html"
+  }
+];
